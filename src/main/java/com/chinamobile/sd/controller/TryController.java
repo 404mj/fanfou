4d29e0cb75561e33900d9a4289c96b7ce692d320
404mj <zhangshuxinmj@163.com>
2019-09-23 15:37:49 +0800

    add log4j2 to logging

--- src/main/java/com/chinamobile/sd/controller/TryController.java
+++ src/main/java/com/chinamobile/sd/controller/TryController.java
@@ -2,6 +2,8 @@ package com.chinamobile.sd.controller;
 
 import com.chinamobile.sd.model.User;
 import com.chinamobile.sd.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,17 +19,21 @@ import java.util.Map;
 @RequestMapping("/try")
 public class TryController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TryController.class);
+
     @Autowired
     private UserService userService;
 
+    //    测试环境配置
     @RequestMapping("/index")
-    public Map<String,String> index() {
-        Map<String,String> a = new HashMap<>();
-        a.put("1","hello");
+    public Map<String, String> index() {
+        Map<String, String> a = new HashMap<>();
+        a.put("1", "hello");
         a.put("2", "world");
         return a;
     }
 
+    //    测试json返回
     @RequestMapping("/array")
     public List<String> array() {
         List<String> b = new ArrayList<>();
@@ -37,20 +43,38 @@ public class TryController {
         return b;
     }
 
-
+    //    测试mysql+mybatis
     @RequestMapping("/alluser")
     public List<User> allUser() {
         return userService.findAllUsers();
     }
 
+    //    测试普通请求参数
     @RequestMapping("/testparam")
     public String testparam(@RequestParam(value = "userName") String userName) {
         return userName;
     }
 
+    //    测试json请求参数
     @RequestMapping("/jsonparam")
     public String jsonParam(@RequestBody String jsonData) {
         return jsonData;
+    }
+
+
+    /**
+     * 测试日志组件
+     * 日志级别
+     * OFF > FATAL > ERROR > WARN > INFO > DEBUG > TRACE > ALL
+     */
+    @RequestMapping("/printLog")
+    public String printLog() {
+        logger.error("ERROR 级别日志");
+        logger.warn("WARN 级别日志");
+        logger.info("INFO 级别日志");
+        logger.debug("DEBUG 级别日志");
+        logger.trace("TRACE 级别日志");
+        return "success";
     }
 
 }
