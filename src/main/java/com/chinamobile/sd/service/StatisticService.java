897cd4cd9c7c3bc7507c794e8925ea48f67d2fd2
404mj <zhangshuxinmj@163.com>
2019-09-28 22:52:05 +0800

    food function 1st step: domian completed!

---
+++ src/main/java/com/chinamobile/sd/service/StatisticService.java
@@ -0,0 +1,10 @@
+package com.chinamobile.sd.service;
+
+/**
+ * @Author: fengchen.zsx
+ * @Date: 2019/9/28 21:44
+ * <p>
+ * 提供统计服务
+ */
+public class StatisticService {
+}
--- src/main/resources/domain.sql
+++ src/main/resources/domain.sql
@@ -1,12 +1,28 @@
 show databases;
+
 create database fanfou;
+
 use fanfou;
 
-create table `user` (
-  `id` int(13) not null primary key  auto_increment comment '主键',
-  `name` varchar(30) default null comment '姓名',
-  `age` int(3) default null comment '年龄',
-  `company` varchar(100) default null comment '公司'
-) engine=InnoDB auto_increment=1 default charset=utf8
+-- 食物详情
+create table `food_detail` (
+  `food_id` int not null  primary key  auto_increment comment '主键',
+  `desc` varchar(30) default null comment '食物名称',
+  `kind` tinyint default null comment '食物的种类:0-菜品;1-面点;2-汤食;3-水果',
+  `reconmmend` tinyint default  null  comment '是否推荐:0-不是推荐的;1-今日推荐',
+  `period` tinyint default null comment '食物所属时段:0-早餐;1-午餐;2-晚餐',
+  `food_time` date default null comment '食物是哪一天的',
+  `up` int default 0 comment '赞的数量',
+  `down` int default 0 comment '踩的数量',
+  KEY `index_get` (`period`,`kind`,`reconmmend`,`food_time`)
+) engine=InnoDB auto_increment=1 default charset=utf8;
 
+
+-- 每日食物评论
+create table `food_discuss` (
+  `discuss_id` int not null primary key auto_increment comment '主键',
+  `discuss_content` varchar(256) default null comment '评论内容',
+  `discuss_time` date default null comment '针对某天食物的评论',
+  KEY `index_day` (`discuss_time`)
+) engine=InnoDB auto_increment=1 default charset=utf8;
 
