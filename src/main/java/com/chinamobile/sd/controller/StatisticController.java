4605586ce2a3deed424e94cc1644257125213204
404mj <zhangshuxinmj@163.com>
2019-10-11 08:40:49 +0800

    fix sql error and construct controller

---
+++ src/main/java/com/chinamobile/sd/controller/StatisticController.java
@@ -0,0 +1,10 @@
+package com.chinamobile.sd.controller;
+
+/**
+ * @Author: fengchen.zsx
+ * @Date: 2019/10/10 9:00
+ * <p>
+ * 处理数据请求
+ */
+public class StatisticController {
+}
--- src/main/resources/domain.sql
+++ src/main/resources/domain.sql
@@ -1,8 +1,16 @@
 show databases;
-
 create database fanfou;
-
 use fanfou;
+
+create table `user`
+(
+  `id`      int(13) not null primary key auto_increment comment '主键',
+  `name`    varchar(30)  default null comment '姓名',
+  `age`     int(3)       default null comment '年龄',
+  `company` varchar(100) default null comment '公司'
+) engine = InnoDB
+  auto_increment = 1
+  default charset = utf8;
 
 -- 食物详情
 create table `food_item`
@@ -27,38 +35,39 @@ create table `food_item`
 -- 每日食物评论
 create table `food_comment`
 (
-  `comment_id`      int not null primary key auto_increment comment '主键',
-  `content`         varchar(512) default null comment '评论内容',
-  `comment_time`    date         default null comment '针对某天食物的评论',
-  `restaurant`      tinyint      default null comment '属于哪个餐厅:0-B1大餐厅;1-B1小餐厅',
-  `discusser`       varchar(20)  default null comment '评论人',
-  UNIQUE `index_day` (`content`, `comment_time`,`restaurant`)
+  `comment_id`   int not null primary key auto_increment comment '主键',
+  `content`      varchar(512) default null comment '评论内容',
+  `comment_time` date         default null comment '针对某天食物的评论',
+  `restaurant`   tinyint      default null comment '属于哪个餐厅:0-B1大餐厅;1-B1小餐厅',
+  `discusser`    varchar(20)  default null comment '评论人',
+  UNIQUE `index_day` (`content`, `comment_time`, `restaurant`)
 ) engine = InnoDB
   auto_increment = 1
   default charset = utf8;
 
 -- 开发过程修改
-insert into food_item (`desc`, kind, reconmmend, period, food_time, food_week, up, down, food_belng)
+insert into food_item (food_desc, kind, recommend, period, food_time, food_week, up, down, food_belng)
 values ('红烧排骨', 0, 1, 1, '2019-09-30', 1, 0, 0, 0),
        ('馒头', 1, 0, 1, '2019-09-30', 1, 0, 0, 0),
        ('老厨白菜', 1, 1, 1, '2019-09-30', 1, 0, 0, 0),
        ('小米粥', 2, 0, 2, '2019-09-30', 1, 0, 0, 0);
-insert into food_item (`desc`, kind, reconmmend, period, food_time, food_week, up, down, food_belng)
+insert into food_item (food_desc, kind, recommend, period, food_time, food_week, up, down, food_belng)
 values ('豆腐脑', 2, 1, 0, '2019-09-30', 1, 0, 0, 0),
        ('煎饼果子', 1, 0, 0, '2019-09-30', 1, 0, 0, 0),
        ('西葫芦水饺', 1, 0, 2, '2019-09-30', 1, 0, 0, 0),
        ('火龙果', 3, 0, 2, '2019-09-30', 1, 0, 0, 0);
 
-insert into food_comment (content, comment_time, discusser)
-VALUES ('白菜有点儿烂了，也可能是焖的有点久了', '2019-09-30', ''),
-       ('小米粥一直是我的最爱！！！~~~', '2019-09-30', ''),
-       ('红烧排骨太硬了了！赞赞赞！', '2019-09-30', '');
+insert into food_comment (content, comment_time, restaurant, discusser)
+VALUES ('白菜有点儿烂了，也可能是焖的有点久了', '2019-09-30', 0, ''),
+       ('小米粥一直是我的最爱！！！~~~', '2019-09-30', 0, ''),
+       ('红烧排骨太硬了了！赞赞赞！', '2019-09-30', 0, '');
 
-insert into food_comment (content, comment_time, discusser)
-VALUES ('永远抢不到的煎饼果子和水饺', '2019-09-30', '');
+insert into food_comment (content, comment_time, restaurant, discusser)
+VALUES ('永远抢不到的煎饼果子和水饺', '2019-09-30', 0, '');
 
 alter table food_item
   change reconmmend recommend tinyint default null comment '是否推荐:0-不是推荐的;1-今日推荐';
 alter table food_item
   modify column food_belng tinyint default null comment '属于哪个餐厅:0-B1大餐厅;1-B1小餐厅';
-alter table food_comment add `restaurant` tinyint      default null comment '属于哪个餐厅:0-B1大餐厅;1-B1小餐厅';
+alter table food_comment
+  add `restaurant` tinyint default null comment '属于哪个餐厅:0-B1大餐厅;1-B1小餐厅';
